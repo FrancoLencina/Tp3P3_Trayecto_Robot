466b4ca26b00a2d45f2a2548bbbc5abaaaf58dfa
Franco Lencina <franlencinaa96@gmail.com>
2025-05-27 15:42:22 -0300

    template solution + backtracking

--- src/model/Solution.java
+++ src/model/Solution.java
@@ -1,20 +1,42 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-	private List<Cell> journey;
+	private List<int[]> _journey;
+	private int _charge;	
 	
-	public void addCell(Cell cell) {
-		
+	public Solution() {
+		_journey = new ArrayList<>();
+		setCharge(0);
 	}
 	
-	public void removeCell() {
-		
+	public void addStep(int x, int y, int charge) {
+		_journey.add(new int[] {x,y});
+		setCharge(getCharge() + charge);
 	}
 	
-	public boolean availableJourney() {  // |carga actual| <= pasos restantes
-		
-		return false;
+	public void removeLastStep(int x, int y, int charge) {
+	    if (!_journey.isEmpty()) {
+	    	_journey.remove(_journey.size() - 1);
+            setCharge(getCharge() - charge);
+        }
 	}
+
+	public int getCharge() {
+		return _charge;
+	}
+
+	public void setCharge(int _charge) {
+		this._charge = _charge;
+	}
+	
+	public Solution clone() {
+		
+        Solution clone = new Solution();
+        clone._journey = new ArrayList<>(_journey);
+        clone.setCharge(_charge);
+        return clone;
+    }
 }
--- src/model/Solver.java
+++ src/model/Solver.java
@@ -1,29 +1,57 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solver {
 
-	private int n; //ancho
-	private int m; //alto
-	private Solution actual;
-	private List<Solution> solutions;
-	private Cell[][] cellMatrix;
+	private int _n; //ancho
+	private int _m; //alto
+	private Solution _actual;
+	private List<Solution> _solutions;
+	private int[][] _matrix;
+	private int _steps;
 	
-	public Solver(int n, int m) {
-		this.n = n;
-		this.m = m;
+	public Solver(int [][] matrix) {
+		_matrix = matrix;
+		_n = matrix.length;
+		_m = matrix[0].length;
+		_steps = 0;
 	}
 	
-	public void resolver(Cell cell){
+	public void solve(){
+		_solutions = new ArrayList<Solution>();
+		_actual = new Solution();
+		_actual.addStep(0, 0, _matrix[0][0]);
+		generateFrom(0, 0, _actual);
+	}
+	
+	public void generateFrom(int row, int col, Solution sol){
+		if(remainingSteps() == 0) {
+			_solutions.add(sol.clone());
+			return;
+		}
 		
-	}
-	
-	public void generateFrom(Cell cell){
+		_steps++;
 		
+        if (row + 1 < _n) {
+            sol.addStep(row + 1, col, _matrix[row + 1][col]);
+            generateFrom(row + 1, col, sol);
+            sol.removeLastStep(row + 1, col, _matrix[row + 1][col]);
+        }
+
+        if (col + 1 < _m) {
+            sol.addStep(row, col + 1, _matrix[row][col + 1]);
+            generateFrom(row, col + 1, sol);
+            sol.removeLastStep(row, col + 1, _matrix[row][col + 1]);
+        }
 	}
 	
+	private int remainingSteps() {
+		return _steps - (_n + (_m - 1));
+	}
+
 	public int solutionsSize(){
-		return solutions.size();
+		return _solutions.size();
 	}
 }
