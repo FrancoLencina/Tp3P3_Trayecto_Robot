a2cc4211df646f5cddcf6f7fe43aa5c95905f565
Matías Duarte <duarte.lmd28@gmail.com>
2025-05-30 17:17:31 -0300

    Backtracking funcionando correctamente

--- src/model/Solver.java
+++ src/model/Solver.java
@@ -10,13 +10,13 @@ public class Solver {
 	private Solution _actual;
 	private List<Solution> _solutions;
 	private int[][] _matrix;
-	private int _steps;
+	private int _remainingSteps;
 	
 	public Solver(int [][] matrix) {
 		_matrix = matrix;
-		_n = matrix.length;
-		_m = matrix[0].length;
-		_steps = 0;
+		_n = matrix[0].length;
+		_m = matrix.length;
+		_remainingSteps = _n + _m - 2;
 	}
 	
 	public void solve(){
@@ -24,33 +24,59 @@ public class Solver {
 		_actual = new Solution();
 		_actual.addStep(0, 0, _matrix[0][0]);
 		generateFrom(0, 0, _actual);
+		System.out.print("Soluciones encontradas:" + solutionsSize());
 	}
 	
 	public void generateFrom(int row, int col, Solution sol){
-		if(remainingSteps() == 0) {
+		
+		System.out.print("Pasos restantes: " + _remainingSteps);
+		System.out.print(" Carga actual " + sol.getCharge());
+		
+		if(sol.getCharge() == 0 && _remainingSteps == 0) {
 			_solutions.add(sol.clone());
+			System.out.println("\nSolución encontrada!");
 			return;
 		}
 		
-		_steps++;
 		
-        if (row + 1 < _n) {
-            sol.addStep(row + 1, col, _matrix[row + 1][col]);
-            generateFrom(row + 1, col, sol);
-            sol.removeLastStep(row + 1, col, _matrix[row + 1][col]);
-        }
-
-        if (col + 1 < _m) {
-            sol.addStep(row, col + 1, _matrix[row][col + 1]);
-            generateFrom(row, col + 1, sol);
-            sol.removeLastStep(row, col + 1, _matrix[row][col + 1]);
-        }
+		if (Math.abs(sol.getCharge()) <= _remainingSteps ) {
+			
+			if (row+1 < _m ) {
+				
+				sol.addStep(row + 1, col, _matrix[row + 1][col]);
+				_remainingSteps-=1;
+				System.out.print(" v\n");
+				generateFrom(row + 1, col, sol);
+				sol.removeLastStep(row+1, col, _matrix[row + 1][col]);
+				_remainingSteps+=1;
+				System.out.print(" ^");
+			}
+			
+			if (col+1 < _n) {
+				
+				sol.addStep(row, col + 1, _matrix[row][col + 1]);
+				_remainingSteps-=1;
+				System.out.print(" ->\n");
+				generateFrom(row, col + 1, sol);
+				sol.removeLastStep(row, col + 1, _matrix[row][col + 1]);
+				_remainingSteps+=1;
+				System.out.print(" <-");
+			}
+		}
+		
+//        if (row + 1 < _n) {
+//            sol.addStep(row + 1, col, _matrix[row + 1][col]);
+//            generateFrom(row + 1, col, sol);
+//            sol.removeLastStep(row + 1, col, _matrix[row + 1][col]);
+//        }
+//
+//        if (col + 1 < _m) {
+//            sol.addStep(row, col + 1, _matrix[row][col + 1]);
+//            generateFrom(row, col + 1, sol);
+//            sol.removeLastStep(row, col + 1, _matrix[row][col + 1]);
+//        }
 	}
 	
-	private int remainingSteps() {
-		return _steps - (_n + (_m - 1));
-	}
-
 	public int solutionsSize(){
 		return _solutions.size();
 	}
