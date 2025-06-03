ddec4fa2a934d2c4d393822af2a6ec98ce4c0442
Matías Duarte <duarte.lmd28@gmail.com>
2025-06-03 12:46:33 -0300

    Update RandomMatrixGeneratorTest.java

--- src/model/RandomMatrixGeneratorTest.java
+++ src/model/RandomMatrixGeneratorTest.java
@@ -7,8 +7,76 @@ import org.junit.Test;
 public class RandomMatrixGeneratorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testValidSize() {
+		boolean allResultsAreValid = true;
+		
+		for (int i = 0; i < 100; i++) { //probamos si funciona para 100 matrices aleatorias diferentes
+			
+			RandomGenerator generator = new RandomGenerator();
+			RandomMatrixGenerator rmg = new RandomMatrixGenerator(generator);
+			boolean valid = false;
+			int[][] matrix = rmg.generateMatrix();
+			
+			if (matrix.length > 1 && matrix.length < 12 && 
+				matrix[0].length > 1 && matrix[0].length < 12) {
+				
+				valid = true;
+			}
+			
+			allResultsAreValid = allResultsAreValid && valid;
+		}
+		
+		assertTrue(allResultsAreValid);
 	}
-
+	
+	@Test
+	public void testValidValues() {
+		boolean allResultsAreValid = true;
+		
+		for (int i = 0; i < 100; i++) { //probamos si funciona para 100 matrices aleatorias diferentes
+			
+			RandomGenerator generator = new RandomGenerator();
+			RandomMatrixGenerator rmg = new RandomMatrixGenerator(generator);
+			boolean valid = false;
+			int[][] matrix = rmg.generateMatrix();
+			
+			for (int row = 0; row<matrix.length; row++) {
+				for (int col = 0; col<matrix[0].length; col++) {
+					
+					if (matrix[row][col] == 1 || matrix[row][col] == -1) {
+						valid = true;
+					}
+				}
+			}
+			allResultsAreValid = allResultsAreValid && valid;
+		
+		}
+		
+		assertTrue(allResultsAreValid);
+	}
+	
+	@Test
+	public void testCompatibledimensions() {
+		boolean allResultsAreValid = true;
+		
+		for (int i = 0; i < 100; i++) { //probamos si funciona para 100 matrices aleatorias diferentes
+			
+			RandomGenerator generator = new RandomGenerator();
+			RandomMatrixGenerator rmg = new RandomMatrixGenerator(generator);
+			boolean valid = false;
+			int[][] matrix = rmg.generateMatrix();
+			int row = matrix.length;
+			int col = matrix[0].length;
+			
+			if ((row + col) % 2 == 1) {
+				valid = true;
+			}
+			
+			allResultsAreValid = allResultsAreValid && valid;
+		
+		}
+		
+		assertTrue(allResultsAreValid);
+	}
 }
+
